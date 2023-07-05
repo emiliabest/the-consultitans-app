717261b089b347b2c288020701988d6e9420d805
emiliabest <138687104+emiliabest@users.noreply.github.com>
2023-07-05 18:14:52 -0400

    Initial commit

---
+++ src/routes/client/+page.svelte
@@ -0,0 +1,29 @@
+<nav>
+	
+</nav>
+
+<h1>Client Information</h1>
+
+<h2>States</h2>
+
+<p><u><i>New York</p>
+<p><u><i>Connecticut</p>
+<p><u><i>Louisiana</p>
+
+
+
+<style>
+  p {
+    color: blue;
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 1em;
+    font-weight: bold;
+    margin: 4; 
+  }
+
+  h2 {
+    font-family: 'Times New Roman', Times, serif;
+    text-align: left;
+    font-size: 1.5em;
+  }
+</style>
---
+++ src/routes/rec/+page.svelte
@@ -0,0 +1,9 @@
+<nav>
+	
+</nav>
+<script>
+
+</script>
+
+<h1>Recommendations for You</h1>
+<h3>These are some recommendations on what to do in case of a </h3>
